ae95f0ed18087c1952de7eb236f665a9c1ba0bc0
AndreyKapus <kapustnikov@ukr.net>
2023-08-29 15:25:21 +0300

    lang dropdown

--- src/Components/Header/ChangeLanguege.js
+++ src/Components/Header/ChangeLanguege.js
@@ -1,18 +1,22 @@
+
+
 import { SelectWrapper,
      Options,
      OptionsWrapper,
      SelectBtn
     } from './Header.styled.js'
 
-export const ChangeLanguege = ({changeLanguage, lang}) => {
+export const ChangeLanguege = ({changeLanguage, lang, toggleDropDown, dropDownIsOpen}) => {
 
     return (
         <SelectWrapper>
-            <SelectBtn onChange={(e) => changeLanguage(e.value)}>{lang}</SelectBtn>
-            <OptionsWrapper>
-                <Options onClick={() => changeLanguage('en')} value='en'>en</Options>
-                <Options onClick={() => changeLanguage('ua')} value='ua'>ua</Options>
-            </OptionsWrapper>     
+            <SelectBtn onChange={(e) => changeLanguage(e.value)} onClick={toggleDropDown}>{lang}</SelectBtn>
+            {dropDownIsOpen && 
+                <OptionsWrapper>
+                    <Options onClick={() => changeLanguage('en')} value='en'>en</Options>
+                    <Options onClick={() => changeLanguage('ua')} value='ua'>ua</Options>
+                </OptionsWrapper>   
+            }
         </SelectWrapper>
     )
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,15 +8,22 @@ import SearchInput from './SearchInput';
 import { useTranslation } from 'react-i18next';
 import ChangeLanguege from './ChangeLanguege';
 import { useLang } from '../../Store/LanguegeStore.js';
+import { useState } from 'react';
 
 const Header = () => {
     const {t, i18n} = useTranslation();
     const lang = useLang((state) => state.lang);
-    const changeLeng = useLang(state => state.setLang)
+    const changeLeng = useLang(state => state.setLang);
+    const [dropDownIsOpen, setDropDownIsOpen] = useState(false)
+
+    const toggleDropDown = (e) => {
+        dropDownIsOpen ? setDropDownIsOpen(false) : setDropDownIsOpen(true);
+    };
 
     const changeLanguage = (language) => {
         i18n.changeLanguage(language);
-        changeLeng(language)
+        changeLeng(language);
+        setDropDownIsOpen(false)
     };
 
     return (
@@ -25,7 +32,7 @@ const Header = () => {
                     <Wrapper>
                         <Title to='/'>{t('title')}</Title>
                     </Wrapper>
-                    <ChangeLanguege lang={lang} changeLanguage={changeLanguage}/>
+                    <ChangeLanguege lang={lang} dropDownIsOpen={dropDownIsOpen} changeLanguage={changeLanguage} toggleDropDown={toggleDropDown}/>
                     <SearchInput/>
                 </HeaderWrapper>
                 <Outlet/>
