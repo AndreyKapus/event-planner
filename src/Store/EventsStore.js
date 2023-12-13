4d94adf8008e7fdfa6f1ca39b0c34277af685d92
AndreyKapus <kapustnikov@ukr.net>
2023-12-13 14:59:47 +0200

    initial store

--- src/Store/EventsStore.js
+++ src/Store/EventsStore.js
@@ -1,45 +1,63 @@
-import { create } from 'zustand';
-import { v4 as uuidv4 } from 'uuid';
-import {devtools, persist, createJSONStorage} from 'zustand/middleware';
+import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
+import { devtools, persist, createJSONStorage } from "zustand/middleware";
+import initialSrore from "./initialStore";
 
-export const useEvents = create(devtools(persist((set, get) => ({
-    events: [],
-    
-    addEvent: ({title, description, startDate, time, location, category, picture, priority}) => set(state => {
-        const newEvent = {
-            id: uuidv4(),
-            title: title,
-            description: description,
-            date: startDate.getDate(),
-            month: startDate.getMonth(),
-            time: time,
-            location: location,
-            category: category,
-            addPicture: picture,
-            priority: priority,
-        };
+export const useEvents = create(
+  devtools(
+    persist(
+      (set, get) => ({
+        events: initialSrore,
 
-       return { events: [...state.events, newEvent] }
+        addEvent: ({
+          title,
+          description,
+          startDate,
+          time,
+          location,
+          category,
+          picture,
+          priority,
+        }) =>
+          set((state) => {
+            const newEvent = {
+              id: uuidv4(),
+              title: title,
+              description: description,
+              date: startDate.getDate(),
+              month: startDate.getMonth(),
+              time: time,
+              location: location,
+              category: category,
+              addPicture: picture,
+              priority: priority,
+            };
+
+            return { events: [...state.events, newEvent] };
+          }),
+
+        deleteEvent: (eId) =>
+          set((state) => {
+            return {
+              events: state.events.filter((ev) => ev.id !== eId),
+            };
+          }),
+      }),
+      {
+        name: "events-storage",
+        storage: createJSONStorage(() => localStorage),
+      }
+    )
+  )
+);
+
+export const useId = create((set) => ({
+  id: "",
+  setId: (value) =>
+    set({
+      id: value,
     }),
-
-    deleteEvent: (eId) => set(state => {
-        return {
-            events: state.events.filter(ev => ev.id !== eId)
-        }
-    }),
-}),
-    {
-    name: 'events-storage',
-    storage: createJSONStorage(() => localStorage),
-  })));
-
-
-  export const useId = create(set => ({
-    id: '',
-    setId: (value) => set({
-        id: value
-    }),
-    setInitialId: () => ({
-        id: ''
-    }),
-  }));
+  setInitialId: () => ({
+    id: "",
+  }),
+}));
---
+++ src/Store/initialStore.js
@@ -0,0 +1,60 @@
+const initialSrore = [
+  {
+    id: "42630d2c-870d-4001-bfd6-f5c504cb9e01",
+    title: "iPhone 15 launch event",
+    description:
+      "Apple always holds an event in September, and almost always uses this to announce a new iPhone. (The only exception in recent memory was in 2020, as a result of Covid-related production issues.",
+    date: 12,
+    month: 8,
+    time: "19:00",
+    location: "Online",
+    category: "Business",
+    addPicture:
+      "https://event-planner-rosy.vercel.app/static/media/conference.55c90d5157a89bdcf16c.jpg",
+    priority: "Low",
+  },
+  {
+    id: "6d6a3cc1-f9fb-4020-a922-85b018a63e0a",
+    title: "RBS Six Nations Rugby",
+    description:
+      "The Six Nations Championship for England, France, Ireland, Italy, Scotland and Wales will see matches take place at venues including the Principality Stadium in Cardiff, Twickenham Stadium in London, and Murrayfield Stadium in Edinburgh. ",
+    date: 2,
+    month: 1,
+    time: "12:00",
+    location: "London",
+    category: "Sport",
+    addPicture:
+      "https://event-planner-rosy.vercel.app/static/media/sport.5ca4f3bc53630885c229.jpg",
+    priority: "Medium",
+  },
+  {
+    id: "98a24e4a-4e29-4a33-b9a5-97e252d40676",
+    title: "Coronation of King Charles III",
+    description:
+      "Charles III will be crowned king at Westminster Abbey by the Archbishop of Canterbury on May 6, marking the first coronation of a British monarch in the 21st century and the first since Queen Elizabeth II in 1953. ",
+    date: 25,
+    month: 11,
+    time: "10:00",
+    location: "Norfolk",
+    category: "Conference",
+    addPicture:
+      "https://event-planner-rosy.vercel.app/static/media/workshop.ece755a9653d80be80bd.jpg",
+    priority: "High",
+  },
+  {
+    id: "4c173f9d-a33e-4f39-a4ae-863a0de7c89a",
+    title: "Burning Man",
+    description:
+      "The only way to describe Burning Man is to experience yourself in person. See you on the playa!",
+    date: 27,
+    month: 7,
+    time: "12:00",
+    location: "USA",
+    category: "Music",
+    addPicture:
+      "https://event-planner-rosy.vercel.app/static/media/music.9e6072212b814af3261d.jpg",
+    priority: "High",
+  },
+];
+
+export default initialSrore;
